ce618a0c17ccbc01a0338d996af55ca6ae0a2f91
harmanjot2002 <harmanjotkainth@gmail.com>
2022-09-24 13:50:56 +0530

    Detect cycle in a directed graph

--- Graphs/detectCycleInDirectedGraph.cpp
+++ Graphs/detectCycleInDirectedGraph.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h> 
 using namespace std; 
 
-bool DFSRec(vector<int> adj[], int s,bool visited[], int parent) 
+bool DFSRec(vector<int> adj[], int s,bool visited[], bool recSt[]) 
 { 	
     visited[s]=true;
+    recSt[s]=true;
     
     for(int u:adj[s]){
-        if(visited[u]==false){
-            if(DFSRec(adj,u,visited,s)==true)
+        if(visited[u]==false && DFSRec(adj,u,visited,recSt)==true)
                 {return true;}
-        }
-        else if(u!=parent)
+        else if(recSt[u]==true)
             {return true;}
     }
+    recSt[s]=false;
     return false;
 }
 
@@ -20,10 +20,13 @@ bool DFS(vector<int> adj[], int V){
     bool visited[V]; 
 	for(int i=0;i<V; i++) 
 		visited[i] = false;
+	bool recSt[V]; 
+	for(int i=0;i<V; i++) 
+		recSt[i] = false;
 		
     for(int i=0;i<V;i++){
         if(visited[i]==false)
-            if(DFSRec(adj,i,visited,-1)==true)
+            if(DFSRec(adj,i,visited,recSt)==true)
                 return true;
     }
     return false;
@@ -31,7 +34,6 @@ bool DFS(vector<int> adj[], int V){
 
 void addEdge(vector<int> adj[], int u, int v){
     adj[u].push_back(v);
-    adj[v].push_back(u);
 }
 
 int main() 
@@ -39,11 +41,11 @@ int main()
 	int V=6;
 	vector<int> adj[V];
 	addEdge(adj,0,1); 
-	addEdge(adj,1,2); 
-	addEdge(adj,2,4); 
-	addEdge(adj,4,5); 
-	addEdge(adj,1,3);
-	addEdge(adj,2,3);
+	addEdge(adj,2,1); 
+	addEdge(adj,2,3); 
+	addEdge(adj,3,4); 
+	addEdge(adj,4,5);
+	addEdge(adj,5,3);
 
 	if(DFS(adj,V))
 	    cout<<"Cycle found";
