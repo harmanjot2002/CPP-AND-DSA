d4e10f52318a5e37768f1f2039d59ec42d29624d
harmanjot2002 <harmanjotkainth@gmail.com>
2022-09-25 14:19:31 +0530

    Shortest Path in DAG

--- Graphs/prim'sAlgorithm.cpp
+++ Graphs/prim'sAlgorithm.cpp
@@ -1,41 +1,71 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-#define V 4
+#include<bits/stdc++.h>
+using namespace std;
 
-int primMST(int graph[V][V]) 
-{ 
+#define V 6		//No of vertices
 
-	int key[V];int res=0; 
-	fill(key,key+V,INT_MAX);
-	bool mSet[V]; key[0]=0;
+int selectMinVertex(vector<int>& value,vector<bool>& setMST)
+{
+	int minimum = INT_MAX;
+	int vertex;
+	for(int i=0;i<V;++i)
+	{
+		if(setMST[i]==false && value[i]<minimum)
+		{
+			vertex = i;
+			minimum = value[i];
+		}
+	}
+	return vertex;
+}
 
-	for (int count = 0; count < V ; count++) 
-	{ 
-		int u = -1; 
+void findMST(int graph[V][V])
+{
+	int parent[V];		//Stores MST
+	vector<int> value(V,INT_MAX);	//Used for edge relaxation
+	vector<bool> setMST(V,false);	//TRUE->Vertex is included in MST
 
-		for(int i=0;i<V;i++)
-		    if(!mSet[i]&&(u==-1||key[i]<key[u]))
-		        u=i;
-		mSet[u] = true; 
-		res+=key[u];
+	//Assuming start point as Node-0
+	parent[0] = -1;	//Start node has no parent
+	value[0] = 0;	//start node has value=0 to get picked 1st
 
-		
-		for (int v = 0; v < V; v++) 
+	//Form MST with (V-1) edges
+	for(int i=0;i<V-1;++i)
+	{
+		//Select best Vertex by applying greedy method
+		int U = selectMinVertex(value,setMST);
+		setMST[U] = true;	//Include new Vertex in MST
 
-			if (graph[u][v]!=0 && mSet[v] == false) 
-				key[v] = min(key[v],graph[u][v]); 
-	} 
-    return res;
-} 
+		//Relax adjacent vertices (not yet included in MST)
+		for(int j=0;j<V;++j)
+		{
+			/* 3 constraints to relax:-
+			      1.Edge is present from U to j.
+			      2.Vertex j is not included in MST
+			      3.Edge weight is smaller than current edge weight
+			*/
+			if(graph[U][j]!=0 && setMST[j]==false && graph[U][j]<value[j])
+			{
+				value[j] = graph[U][j];
+				parent[j] = U;
+			}
+		}
+	}
+	//Print MST
+	for(int i=1;i<V;++i)
+		cout<<"U->V: "<<parent[i]<<"->"<<i<<"  wt = "<<graph[parent[i]][i]<<"\n";
+}
 
-int main() 
-{ 
-	int graph[V][V] = { { 0, 5, 8, 0}, 
-						{ 5, 0, 10, 15 }, 
-						{ 8, 10, 0, 20 }, 
-						{ 0, 15, 20, 0 },}; 
+int main()
+{
+	int graph[V][V] = { {0, 4, 6, 0, 0, 0},
+						{4, 0, 6, 3, 4, 0},
+						{6, 6, 0, 1, 8, 0},
+						{0, 3, 1, 0, 2, 3},
+						{0, 4, 8, 2, 0, 7},
+						{0, 0, 0, 3, 7, 0} };
 
-	cout<<primMST(graph); 
+	findMST(graph);	
+	return 0;
+}
 
-	return 0; 
-} 
+//TIME COMPLEXITY: O(V^2)
