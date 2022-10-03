53bed56294c7e6fa387949253508a071209ca47d
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-03 09:52:34 +0530

    Insert At Middle

---
+++ Arrays/insertAtMiddle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int insertMid(int arr[],int n,int x){
+    int mid=n/2;
+    for(int i=n-1;i>=mid;i--){
+        arr[i+1]=arr[i];
+    }
+    arr[mid]=x;
+    for(int i=0;i<n+1;i++){
+        cout<<arr[i]<<" "; 
+    }
+}
+
+int main(){
+    int arr[50],n,x;
+    cout<<"Enter number of elements in array"<<endl;
+    cin>>n;
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter element to be inserted"<<endl;
+    cin>>x;
+    insertMid(arr,n,x);
+    return 0;
+}
