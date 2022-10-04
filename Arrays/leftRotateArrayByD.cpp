3405080ba2613f624f0201f59e5a7f97bddc0673
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-04 08:44:53 +0530

    Left Rotate Array By D

---
+++ Arrays/leftRotateArrayByD.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse(int arr[],int low,int high){
+    while (low<high){
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    }
+}
+
+void leftByD(int arr[],int n,int d){
+    reverse(arr,0,d-1);
+    reverse(arr,d,n-1);
+    reverse(arr,0,n-1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    int arr[50],n,d;
+    cout<<"Enter number of elements in array"<<endl;
+    cin>>n;
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter number of elements to be rotated"<<endl;
+    cin>>d;
+    leftByD(arr,n,d);
+     
+    return 0;
+}
