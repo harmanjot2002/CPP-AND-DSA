dc956104862c763c5e7850a96dee44eca2ec449e
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-04 10:50:58 +0530

    Pair Sum

---
+++ Arrays/pairSum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int pairSum(int arr[],int n,int x){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==x){
+                cout<<arr[i]<<" "<<arr[j]<<endl;
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    pairSum(arr,5,6);     
+    return 0;
+}
