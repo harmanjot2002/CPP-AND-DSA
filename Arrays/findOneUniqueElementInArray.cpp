e0848e700b2bee2f4eff9bce0a7dec7c1ef84cc7
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-04 10:46:48 +0530

    Find One Unique Element In An Array

---
+++ Arrays/findOneUniqueElementInArray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findUnique(int arr[],int n){
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans=ans^arr[i];
+    }
+    return ans;
+}
+
+int main(){
+    int arr[50],n;
+    cout<<"Enter number of elements in array"<<endl;
+    cin>>n;
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<findUnique(arr,n);     
+    return 0;
+}
+
+//Time Complexity: O(n)
