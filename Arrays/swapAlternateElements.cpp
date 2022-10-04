efb696758a4aa71345fed5a824f1bc912f2ac4e6
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-04 10:47:37 +0530

    Swap Alternate Elements In An Array

---
+++ Arrays/swapAlternateElements.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swapAlternates(int arr[],int n){
+    for(int i=0;i<n;i+=2){
+        if(i+1<n){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[50],n;
+    cout<<"Enter number of elements in array"<<endl;
+    cin>>n;
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    swapAlternates(arr,n);    
+    return 0;
+}
+
+//Time Complexity: O(n)
