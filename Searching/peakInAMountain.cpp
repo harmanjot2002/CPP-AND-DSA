91021763a4284888d7de716b7adb21955e864035
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-06 15:00:26 +0530

    Peak in a mountain

---
+++ Searching/peakInAMountain.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int peak(int arr[],int n){
+    int s=0;
+    int e=n-1;
+    int mid=s+(e-s)/2;
+    while(s<e){
+        if(arr[mid]<arr[mid+1]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
+    }
+    return s;
+}
+
+int main(){
+   int arr[50],n,x;
+    cout<<"Enter number of elements in array"<<endl;
+    cin>>n;
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    cout<<peak(arr,n);
+    return 0;
+}
