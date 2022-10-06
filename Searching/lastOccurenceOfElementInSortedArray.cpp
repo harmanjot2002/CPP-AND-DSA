22b4b75de96fe29566c79ee83f51d1672812ff06
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-06 06:42:06 +0530

    Last Occurence Of Element In Sorted Array

---
+++ Searching/lastOccurenceOfElementInSortedArray.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lastOcc(int arr[],int n,int x){
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]<x){
+            low=mid+1;
+        }
+        else if(arr[mid]>x){
+            high=mid-1;
+        }
+        else{
+            if(mid==n-1 || arr[mid]!=arr[mid+1]){
+                return mid;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[50],n,x;
+    cout<<"Enter number of elements in array"<<endl;
+    cin>>n;
+    cout<<"Enter elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter element to be searched"<<endl;
+    cin>>x;
+    cout<<lastOcc(arr,n,x);
+     
+    return 0;
+}
+
+//Time Complexity: O(log n)
+//Space Complexity: O(1)
