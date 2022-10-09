ac7d7048d36b62bdc7968b6de03e33c3eab8a588
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-09 10:58:24 +0530

    Bubble Sort

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void bubbleSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
