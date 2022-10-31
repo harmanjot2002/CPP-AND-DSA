46eb7efa7a2c17e13d6a4fc5c4b97cf874e1bd3e
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-31 14:58:12 +0530

    STL Stack

--- STL/stack.cpp
+++ STL/stack.cpp
@@ -1,8 +1,17 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
 
 int main(){
-    
-     
+    stack<string> s;
+    s.push("love");
+    s.push("babbar");
+    s.push("kumar");
+
+    cout<<"Top Element: "<<s.top()<<endl;
+    s.pop();
+    cout<<"Top Element: "<<s.top()<<endl;
+    cout<<"Size of stack: "<<s.size()<<endl;
+    cout<<"Empty or not "<<s.empty()<<endl;
     return 0;
 }
