313c80ffc4b0188bbfa1cb4ad5e452ccf6fac2db
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-31 15:01:22 +0530

    STL Queue

---
+++ STL/queue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(){
+    queue<string> q;
+    q.push("Love");
+    q.push("Babbar");
+    q.push("Kumar");
+
+    cout<<"Size before pop: "<<q.size()<<endl;
+    cout<<"First element: "<<q.front()<<endl;
+    q.pop();
+    cout<<"First element: "<<q.front()<<endl;
+    cout<<"Size after pop: "<<q.size()<<endl;
+    return 0;
+}
