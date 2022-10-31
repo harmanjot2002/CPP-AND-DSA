1ae82a1dbbc237229142a3b1afc252d2ea88b430
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-31 14:54:48 +0530

    STL Deque

--- STL/list.cpp
+++ STL/list.cpp
@@ -4,11 +4,20 @@ using namespace std;
 
 int main(){
     list<int> l;
+    list<int> n(5,100);
+     for(int i:n){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
     l.push_back(1);
     l.push_front(2);
     l.push_front(3);
     l.push_front(4);
     l.push_front(5);
+    //l.pop_back();
+    //l.pop_front();
+
 
     for(int i:l){
         cout<<i<<" ";
@@ -22,8 +31,8 @@ int main(){
     }
     cout<<endl;
 
-    cout<<"Front "<<l.front()<<endl;
-    cout<<"Back "<<l.back()<<endl;
-
+    cout<<"Front: "<<l.front()<<endl;
+    cout<<"Back: "<<l.back()<<endl;
+    cout<<"Size: "<<l.size()<<endl;
     return 0;
 }
