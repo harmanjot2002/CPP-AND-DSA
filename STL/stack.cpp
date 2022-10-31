25c70e08990548beb0d7dbfc88c83b1eddbfc026
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-31 15:37:05 +0530

    STL Stack

--- STL/stack.cpp
+++ STL/stack.cpp
@@ -13,5 +13,10 @@ int main(){
     cout<<"Top Element: "<<s.top()<<endl;
     cout<<"Size of stack: "<<s.size()<<endl;
     cout<<"Empty or not "<<s.empty()<<endl;
+
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
     return 0;
 }
