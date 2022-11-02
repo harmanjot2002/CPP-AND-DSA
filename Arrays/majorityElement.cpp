6c5c9deda0b7dcfeb671b2af15f636e89c412fdb
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-02 18:30:24 +0530

    Majority element in an array

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 /* Function to find the candidate for Majority */
 int findCandidate(int a[], int size) { 
-	int maj_index = 0, count = 1; 
-	for (int i = 1; i < size; i++) 
-	{ 
+	int maj_index = 0;
+	int count = 1; 
+	for (int i = 1; i < size; i++) { 
 		if (a[maj_index] == a[i]) 
 			count++; 
 		else
@@ -19,45 +19,37 @@ int findCandidate(int a[], int size) {
 	return a[maj_index]; 
 } 
 
-/* Function to check if the candidate occurs more than n/2 times */
-bool isMajority(int a[], int size, int cand) 
-{ 
+/* Function to check if the candidate occurs more than n/2 times,i.e,check whether it is majority element or not */
+bool isMajority(int a[], int size, int cand) { 
 	int count = 0; 
 	for (int i = 0; i < size; i++) 
-	
-	if (a[i] == cand) 
-	count++; 
+		if (a[i] == cand) 
+			count++; 
 		
 	if (count > size/2) 
-	return 1; 
-	
+		return 1; 
 	else
-	return 0; 
+		return 0; 
 } 
 
 /* Function to print Majority Element */
-void printMajority(int a[], int size) 
-{ 
-/* Find the candidate for Majority*/
-int cand = findCandidate(a, size); 
+void printMajority(int a[], int size) { 
+	/* Find the candidate for Majority*/
+	int cand = findCandidate(a, size); 
 
-/* Print the candidate if it is Majority*/
-if (isMajority(a, size, cand)) 
-cout << " " << cand << " "; 
-	
-else
-cout << "No Majority Element"; 
+	/* Print the candidate if it is Majority*/
+	if (isMajority(a, size, cand)) 
+		cout << " " << cand << " "; 
+	else
+		cout << "No Majority Element"; 
 } 
 
 
 /* Driver function to test above functions */
-int main() 
-{ 
+int main() { 
 	int a[] = {1, 3, 3, 1, 2}; 
-	int size = (sizeof(a))/sizeof(a[0]); 
-	
+	int size = 5;
 	// Function calling 
 	printMajority(a, size); 
-	
 	return 0; 
 } 
