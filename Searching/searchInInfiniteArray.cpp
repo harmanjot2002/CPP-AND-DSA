bcc251504b6b77fbfcc33d6fcd979adc9627af2a
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-02 08:15:58 +0530

    Search in Infinite array

--- Searching/searchInInfiniteArray.cpp
+++ Searching/searchInInfiniteArray.cpp
@@ -22,26 +22,17 @@ int search(int arr[], int x)
 {
     if (arr[0] == x)
         return 0;
-
     int i = 1;
-
     while (arr[i] < x)
         i = i * 2;
-
     if (arr[i] == x)
         return i;
-
     return bSearch(arr, i / 2 + 1, i - 1, x);
 }
 
-int main()
-{
-
+int main(){
     int arr[] = {1, 2, 3, 5, 5};
-
     int x = 4;
-
     cout << search(arr, x);
-
     return 0;
 }
