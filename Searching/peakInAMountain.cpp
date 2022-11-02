c5661033e180f10cbba2b56e0af491b33327ab94
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-02 08:15:40 +0530

    Peak in a mountain

--- Searching/peakInAMountain.cpp
+++ Searching/peakInAMountain.cpp
@@ -4,15 +4,14 @@ using namespace std;
 int peak(int arr[],int n){
     int s=0;
     int e=n-1;
-    int mid=s+(e-s)/2;
     while(s<e){
+        int mid=s+(e-s)/2;
         if(arr[mid]<arr[mid+1]){
             s=mid+1;
         }
         else{
             e=mid;
         }
-        mid=s+(e-s)/2;
     }
     return s;
 }
