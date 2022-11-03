43c506f33d98899a9175f0f2a9d3d06081b4cb16
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-03 06:59:39 +0530

    Selection Sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,15 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//In selection sort,we try to place smallest element at its correct position.
+//Time Complexity:O(n^2)
+//Not Stable
+//In-place
+
 void selectionSort(int arr[],int n){
-    int min_ind;
-    for (int i=0;i<n-1;i++){
-        min_ind=i;
+    for(int i=0;i<n;i++){
+        int min_ind=i;
         for(int j=i+1;j<n;j++){
-            if(arr[j]<arr[min_ind])
+            if(arr[j]<arr[min_ind]){
                 min_ind=j;
-            
-            
+            }
         }
         swap(arr[min_ind],arr[i]);
     }
