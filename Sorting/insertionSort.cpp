bc0e020e4c4c3d85eb216b2e0955cd8f80b309bc
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-03 06:59:17 +0530

    Insertion Sort

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -1,15 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Here,we maintain a part of sorted array at one side and then insert other elements from unsorted part at its correct position.
+//Time Complexity:O(n^2)
+//Stable
+//In-place
+
 int insertionSort(int arr[],int n){
     for(int i=1;i<n;i++){
-        int key=arr[i];
+        int temp=arr[i];
         int j=i-1;
-        while(j>=0 && arr[j]>key){
+        while(j>=0 && arr[j]>temp){
             arr[j+1]=arr[j];
             j--;
         }
-        arr[j+1]=key;
+        arr[j+1]=temp;
     }
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
