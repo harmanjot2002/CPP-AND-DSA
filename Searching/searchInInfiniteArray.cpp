1eb94920c2c4f601fe564cef7bfdffccfb813c01
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-04 09:30:40 +0530

    Search in an Infinte Sorted array

--- Searching/searchInInfiniteArray.cpp
+++ Searching/searchInInfiniteArray.cpp
@@ -1,25 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int bSearch(int arr[], int low, int high, int x)
-{
+int bSearch(int arr[], int low, int high, int x){
     if (low > high)
         return -1;
-
     int mid = (low + high) / 2;
-
     if (arr[mid] == x)
         return mid;
-
     else if (arr[mid] > x)
         return bSearch(arr, low, mid - 1, x);
-
     else
         return bSearch(arr, mid + 1, high, x);
 }
 
-int search(int arr[], int x)
-{
+int search(int arr[], int x){
     if (arr[0] == x)
         return 0;
     int i = 1;
