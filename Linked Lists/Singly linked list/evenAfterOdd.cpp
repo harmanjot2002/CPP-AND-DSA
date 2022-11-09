b1ef4db53e343fa1512ba3d09cc51580fb29153b
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-09 08:37:41 +0530

    Put even nodes after odd nodes

--- Linked Lists/Singly linked list/evenAfterOdd.cpp
+++ Linked Lists/Singly linked list/evenAfterOdd.cpp
@@ -1,49 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Node{
     int data;
-    Node* next;
+    Node *next;
     Node(int x){
-        data=x;
-        next=NULL;
+        data = x;
+        next = NULL;
     }
 };
 
-void printList(Node* head){
-    while(head!=NULL){
-        cout<<head->data<<" ";
-        head=head->next;
+Node *evenAfterOdd(Node *head){
+    Node *odd = head;
+    Node *even = head->next;
+    Node *evenStart = even;
+    while (odd->next != NULL && even->next != NULL){
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = evenStart;
+    if (odd->next != NULL){
+        even->next = NULL;
     }
 }
 
-Node* evenAfterOdd(Node* head){
-   Node* even=head->next;
-   Node* odd=head;
-   Node* evenStart=even;
-   while(odd->next!=NULL && even->next!=NULL){
-    odd->next=even->next;
-    odd=odd->next;
-    even->next=odd->next;
-    even=even->next;
-   }
-   odd->next=evenStart;
-   if(odd->next!=NULL){
-    even->next=NULL;
-   }
+void printList(Node *head){
+    while (head != NULL){
+        cout << head->data << " ";
+        head = head->next;
+    }
 }
 
 int main(){
-    Node* head=new Node(23);
-    head->next=new Node(29);
-    head->next->next=new Node(34);
-    head->next->next->next=new Node(89);
-    cout<<"Before"<<endl;
+    Node *head = new Node(23);
+    head->next = new Node(29);
+    head->next->next = new Node(34);
+    head->next->next->next = new Node(89);
+    cout << "Before" << endl;
     printList(head);
-    cout<<endl;
+    cout << endl;
     evenAfterOdd(head);
-    cout<<"After after"<<endl;
-    printList(head);  
-    cout<<endl;
+    cout << "After after" << endl;
+    printList(head);
+    cout << endl;
     return 0;
 }
