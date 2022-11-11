0c04fed7e5b68a27bab9abad73e1a7f3eb833f4f
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-11 17:58:01 +0530

    Stack STL

--- Stacks/stackSTL.cpp
+++ Stacks/stackSTL.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
 
 int main(){
-    stack<int>s;
+    stack<int> s;
     s.push(10);
     s.push(20);
     s.push(30);
@@ -11,6 +11,9 @@ int main(){
     cout<<s.top()<<endl;
     s.pop();
     cout<<s.top()<<endl; 
-        
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
     return 0;
 }
