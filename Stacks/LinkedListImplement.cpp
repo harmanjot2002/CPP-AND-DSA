0ed9c146a4a65f66f7313e9250ac1b7ec3cb1559
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-11 17:54:57 +0530

    Linked List Implementation of Stack

--- Stacks/LinkedListImplement.cpp
+++ Stacks/LinkedListImplement.cpp
@@ -1,52 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// struct Node{
-//     int data;
-//     Node* next;
-//     Node(int x){
-//         data=x;
-//         next=NULL;
-//     }
-// };
-// struct MyStack{
-//     Node* head;
-//     int sz;
-//     MyStack(){
-//         head=NULL;
-//         sz=0;
-//     }
-//     void push(int x){
-//         Node* temp=new Node(x);
-//         temp->next=head;
-//         head=temp;
-//         sz++;
-//     }
-//     int pop(){
-//         if(head==NULL){
-//             return INT_MAX;
-//         }
-//         int ans=head->data;
-//         Node* res=head;
-//         head=head->next;
-//         delete(res);
-//         sz--;
-//         return ans;
-//     }
-//     int peek(){
-//          if(head==NULL){
-//             return INT_MAX;
-//         }
-//         return head->data;
-//     }
-//     int size(){
-//         return sz;
-//     }
-//     bool isEmpty(){
-//         return (head==NULL);
-//     }
-// };
-
 struct Node{
     int data;
     Node* next;
@@ -63,12 +17,14 @@ struct MyStack{
         sz=0;
         head=NULL;
     }
+
     void push(int x){
         Node* temp=new Node(x);
         temp->next=head;
         head=temp;
         sz++;
     }
+    
     int pop(){
         if(head==NULL)
             return INT_MAX;
@@ -79,12 +35,15 @@ struct MyStack{
         sz--;
         return res;
     }
+    
     int size(){
         return sz;
     }
+    
     bool isEmpty(){
         return (head==NULL);
     }
+    
     int peek(){
         if(head==NULL)
             return INT_MAX;
