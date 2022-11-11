e69634c5f466392524399280206d1ab4c401e407
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-11 17:59:32 +0530

    Stack Traversal

--- Stacks/stackTraversal.cpp
+++ Stacks/stackTraversal.cpp
@@ -10,5 +10,13 @@ int main(){
         cout<<s.top()<<endl;
         s.pop();
     }
+    
+    s.push(2);
+    s.push(9);
+    s.push(63);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
     return 0;
 }
