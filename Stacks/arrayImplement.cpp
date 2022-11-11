2aa57eebe1fb595c340156f950fc34a2adb68180
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-11 17:42:48 +0530

    Array Implementation of Stack

--- Stacks/arrayImplement.cpp
+++ Stacks/arrayImplement.cpp
@@ -1,62 +1,8 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// struct MyStack{
-//     int* arr;
-//     int cap;
-//     int top;
-//     MyStack(int c){
-//         cap=c;
-//         arr=new int[cap];
-//         top=-1;
-//     }
-//     void push(int x){
-//         if(top==cap-1){
-//             cout<<"Overflow";
-//         }
-//         top++;
-//         arr[top]=x;
-//     }
-//     int pop(){
-//         if(top==-1){
-//             cout<<"Underflow";
-//         }
-//         int res=arr[top];
-//         top--;
-//         return res;
-//     }
-//     int peek(){
-//         if(top==-1){
-//             cout<<"Underflow";
-//         }
-//         return arr[top];
-//     }
-//     int size(){
-//         return (top+1);
-//     }
-//     bool isEmpty(){
-//         return (top==-1);
-//     }
-// };
-
-// int main(){
-
-//     MyStack s(5);
-//     s.push(5);
-//     s.push(10);
-//     s.push(15);
-//     cout<<s.pop()<<endl;
-//     cout<<s.size()<<endl;
-//     cout<<s.peek()<<endl;
-//     cout<<s.isEmpty()<<endl;
-//     return 0;
-// }
-
 #include<iostream>
 using namespace std;
 
 struct MyStack{
-    int* arr;
+    int *arr;
     int cap;
     int top;
     MyStack(int c){
@@ -64,13 +10,15 @@ struct MyStack{
         cap=c;
         top=-1;
     }
+
     void push(int x){
         if(top==cap-1){
-            cout<<"Stack is full";
+            cout<<"Overflow";
         }
         top++;
         arr[top]=x;
     }
+    
     int pop(){
         if(top==-1){
             cout<<"Underflow";
@@ -79,15 +27,18 @@ struct MyStack{
         top--;
         return res;
     }
+    
     int peek(){
         if(top==-1){
             cout<<"Underflow";
         }
         return arr[top];
     }
+    
     int size(){
         return (top+1);
     }
+    
     bool isEmpty(){
         return (top==-1);        
     }
