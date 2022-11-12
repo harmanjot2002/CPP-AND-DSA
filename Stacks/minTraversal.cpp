1bb4c5b55e3a267d5ddff2a814e1a80afcdaec4d
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-12 14:21:08 +0530

    Minimum traversals for balanced parenthesis

--- Stacks/minTraversal.cpp
+++ Stacks/minTraversal.cpp
@@ -1,36 +1,29 @@
-// C++ program to find minimum number of reversals required to balance an expression
+// C++ program to find minimum number of reversals required to balance an stression
 #include <bits/stdc++.h>
 using namespace std;
 
-// Returns count of minimum reversals for making
-// expr balanced. Returns -1 if expr cannot be
-// balanced.
-int countMinReversals(string expr)
-{
-	int len = expr.length();
-
-	// length of expression must be even to make
+int countMinReversals(string str){
+	// length of exression must be even to make
 	// it balanced by using reversals.
-	if (len % 2 != 0)
+	if (str.length()% 2 != 0)
 		return -1;
 
 	// After this loop, stack contains unbalanced
 	// part of expression, i.e., expression of the
 	// form "}}..}{{..{"
 	stack<char> s;
-	for (int i = 0; i < len; i++) {
-		if (expr[i] == '}' && !s.empty()) {
+	for (int i = 0; i <str.length(); i++) {
+		if (str[i] == '}' && !s.empty()) {
 			if (s.top() == '{')
 				s.pop();
 			else
-				s.push(expr[i]);
+				s.push(str[i]);
 		}
 		else
-			s.push(expr[i]);
+			s.push(str[i]);
 	}
 
 	// Length of the reduced expression
-	// red_len = (m+n)
 	int red_len = s.size();
 
 	// count opening brackets at the end of
@@ -40,17 +33,13 @@ int countMinReversals(string expr)
 		s.pop();
 		n++;
 	}
-
-	// return ceil(m/2) + ceil(n/2) which is
-	// actually equal to (m+n)/2 + n%2 when
-	// m+n is even.
+	
 	return (red_len / 2 + n % 2);
 }
 
 // Driver program to test above function
-int main()
-{
-	string expr = "}}{{";
-	cout << countMinReversals(expr);
+int main(){
+	string str= "}}{{";
+	cout << countMinReversals(str);
 	return 0;
 }
