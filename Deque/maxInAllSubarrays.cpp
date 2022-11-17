5377fb1c99d4184b99fc921b03f9ac0597176aaa
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-17 11:17:53 +0530

    Maximum in All Subarrays

--- Deque/maxInAllSubarrays.cpp
+++ Deque/maxInAllSubarrays.cpp
@@ -2,24 +2,31 @@
 using namespace std;
 
 void printKMax(int arr[],int n,int k){
-    deque<int>dq;
-    for(int i=0;i<k;i++){
-        while(!dq.empty() && arr[i>arr[dq.back()]]){
-            dq.pop_back();
+    for(int i=0;i<n-k+1;i++){
+        int mx=arr[i];
+        for(int j=i+1;j<i+k;j++){
+            mx=max(mx,arr[j]);
         }
-        dq.push_back(i);
+        cout<<mx<<" ";
     }
-    for(int i=k;i<n;i++){
-        cout<<arr[dq.front()]<<" ";
-        while(!dq.empty() && dq.front()<=i-k){
-            dq.pop_front();
-        }
-        while(!dq.empty() && arr[i]>=arr[dq.back()]){
-            dq.pop_back();
-        }
-        dq.push_back(i);
-    }
-    cout<<arr[dq.front()]<<" ";
+    // deque<int>dq;
+    // for(int i=0;i<k;i++){
+    //     while(!dq.empty() && arr[i>arr[dq.back()]]){
+    //         dq.pop_back();
+    //     }
+    //     dq.push_back(i);
+    // }
+    // for(int i=k;i<n;i++){
+    //     cout<<arr[dq.front()]<<" ";
+    //     while(!dq.empty() && dq.front()<=i-k){
+    //         dq.pop_front();
+    //     }
+    //     while(!dq.empty() && arr[i]>=arr[dq.back()]){
+    //         dq.pop_back();
+    //     }
+    //     dq.push_back(i);
+    // }
+    // cout<<arr[dq.front()]<<" ";
 }
 
 int main(){
@@ -27,33 +34,3 @@ int main(){
     printKMax(arr,7,3);
     return 0;
 }
-
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// void printKMax(int arr[],int n,int k){
-//     deque<int>dq;
-//     for(int i=0;i<k;i++){
-//         while(!dq.empty() && arr[i]>arr[dq.back()]){
-//             dq.pop_back();
-//         }
-//         dq.push_back(i);
-//     }
-//     for(int i=k;i<n;i++){
-//         cout<<arr[dq.front()]<<" ";
-//         while(!dq.empty() && dq.front()<=i-k){
-//             dq.pop_front();
-//         }
-//         while(!dq.empty() && arr[i]>=arr[dq.back()]){
-//             dq.pop_back();
-//         }
-//         dq.push_back(i);
-//     }
-//     cout<<arr[dq.front()]<<" ";
-// }
-
-// int main(){
-//     int arr[]={10,8,5,12,15,7,6};
-//     printKMax(arr,7,3);
-//     return 0;
-// }
