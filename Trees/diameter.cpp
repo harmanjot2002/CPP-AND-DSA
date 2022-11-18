ca1800e115c32f49e3f94aca3a7f458ef26c9ab4
harmanjot2002 <harmanjotkainth@gmail.com>
2022-11-18 18:57:33 +0530

    Diameter of Binary Tree

--- Trees/diameter.cpp
+++ Trees/diameter.cpp
@@ -11,27 +11,27 @@ struct Node{
     }
 };
 
+int res=0;
 int height(Node* root){
     if(root==NULL)
         return 0;
     int lh=height(root->left);
     int rh=height(root->right);
+    res=max(res,lh+rh+1);
     return max(lh,rh)+1;
-}
-
-int diameter(Node* root){
-    int res=0;
-    int hght=height(root);
-    res=max(res,hght)+1;
 }
 
 int main(){
     Node* root=new Node(10);
     root->left=new Node(20);
-    root->right=new Node(30);
-    root->right->left=new Node(40);
-    root->right->right=new Node(50);
-    root->right->left->left=new Node(60);
-    cout<<diameter(root);
+    root->right=new Node(60);
+    root->left->left=new Node(30);
+    root->left->right=new Node(80);
+    root->left->left->left=new Node(40);
+    root->left->left->right=new Node(50);
+    root->left->left->left->left=new Node(60);
+    root->left->right->right=new Node(90);
+    root->left->right->right->right=new Node(18);
+    cout<<height(root);
     return 0;
 }
