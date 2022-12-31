49ddcd7ae4c2ff6ebf2e45187934b59483a59381
harmanjot2002 <harmanjotkainth@gmail.com>
2022-12-31 12:17:02 +0530

    31 December,2022

---
+++ POTD GFG/minNumberOfLaptopsRequired.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int minLaptops(int N, int start[], int end[]) {
+        // Code here
+        sort(start,start+N);
+        sort(end,end+N);
+        int ans=0;
+        int count=0;
+        int i=0,j=0;
+        while(i<N){
+            if(start[i]<end[j]){
+                count++;
+                ans=max(ans,count);
+                i++;
+            }
+            else{
+                count--;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int start[N], end[N];
+        for(int i=0; i<N; i++)
+            cin>>start[i];
+        for(int i=0; i<N; i++)
+            cin>>end[i];
+            
+        Solution ob;
+        cout << ob.minLaptops(N, start, end) << endl;
+    }
+}
+// } Driver Code Ends
