7b2cf73edcb92b71cdb1212ed06c25f1385ff92e
harmanjot2002 <harmanjotkainth@gmail.com>
2023-01-08 20:29:05 +0530

    8 January,2023

---
+++ POTD GFG/absoluteDifferenceDivisibleByK.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    long long countPairs(int n, int arr[], int k) {
+        
+        long long ans = 0;
+        int mp[k+1]={0};
+        for(int i=0;i<n;i++){
+            ans += mp[arr[i]%k];
+            mp[arr[i]%k]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin>>n;
+        
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        
+        cin>>k;
+
+        Solution ob;
+        cout << ob.countPairs(n,arr,k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
