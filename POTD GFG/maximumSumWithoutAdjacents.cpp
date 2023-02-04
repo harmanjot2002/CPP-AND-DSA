0c82bc0efec8f8e0297d5d40bd1d15b61690baa7
harmanjot2002 <harmanjotkainth@gmail.com>
2023-02-04 11:15:12 +0530

    4 February,2023

---
+++ POTD GFG/maximumSumWithoutAdjacents.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    int ans=0,mx=0;
+        for(int i=0;i<n;i++) {
+            int val = arr[i]+mx;
+            mx = max(mx, ans);
+            ans = max(ans,val);
+        }
+        return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaxSum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
