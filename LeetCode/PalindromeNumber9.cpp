1b501f6f23f0091f782f7c4ac93cea03ae104c99
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-03 19:10:16 +0530

    9.Palindrome Number

---
+++ LeetCode/PalindromeNumber9.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int y=x;
+        int rev=0;
+        while(x!=0){
+            int rem=x%10;
+            if((rev>INT_MAX/10) || (rev<INT_MIN/10)){
+                return 0;
+            }
+            rev=(rev*10)+rem;
+            x=x/10;
+        }
+        if(rev==y && rev>=0)
+            return true;
+        else
+            return false;
+    }
+};
