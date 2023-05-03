d6f2159ff4bc48a9e39737a65ccfa14dd9eb4d18
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-03 19:07:59 +0530

    42.Trapping Rainwater

---
+++ LeetCode/TrappingRainwater42.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int len=height.size();
+        // vector<int> lmax;
+        // vector<int> rmax;
+        int lmax[len];
+        int rmax[len];
+        lmax[0]=height[0];
+        for(int i=1;i<len;i++){
+            lmax[i]=max(lmax[i-1],height[i]);
+        }
+        rmax[len-1]=height[len-1];
+        for(int i=len-2;i>=0;i--){
+            rmax[i]=max(rmax[i+1],height[i]);
+        }
+        int sum=0;
+        for(int i=1;i<len-1;i++){
+            sum=sum+(min(lmax[i],rmax[i])-height[i]);
+        }
+        cout << sum;
+        return sum;
+    }
+};
