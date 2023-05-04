4635f5d8921b9e6257a61680cf64069bcbcc85af
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-04 12:49:20 +0530

    7.Reverse Integer

--- LeetCode/7ReverseInteger.cpp
+++ LeetCode/7ReverseInteger.cpp
@@ -5,7 +5,7 @@ public:
         while(x!=0){
             int rem=x%10;
             if((rev>INT_MAX/10) || (rev<INT_MIN/10)){
-                return 0;
+                return 0; 
             }
             rev=(rev*10)+rem;
             x=x/10;
