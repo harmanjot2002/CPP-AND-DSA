685fed392279a0eb689f8849e2e9d2f1e4dcd828
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-04 12:49:48 +0530

    9.Palindrome Number

--- LeetCode/9PalindromeNumber.cpp
+++ LeetCode/9PalindromeNumber.cpp
@@ -8,7 +8,7 @@ public:
             if((rev>INT_MAX/10) || (rev<INT_MIN/10)){
                 return 0;
             }
-            rev=(rev*10)+rem;
+            rev=(rev*10)+rem; 
             x=x/10;
         }
         if(rev==y && rev>=0)
