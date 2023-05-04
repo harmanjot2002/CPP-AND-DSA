46526e1dd2f4b4df543f25cd0397ecc8f0e9e148
Harmanjot Kaur <113688728+harmanjot2002@users.noreply.github.com>
2023-05-04 23:24:53 +0530

    75.Sort Colors

---
+++ LeetCode/75SortColors.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]>nums[j])
+                    swap(nums[i],nums[j]);
+            }
+        }
+    }
+};
